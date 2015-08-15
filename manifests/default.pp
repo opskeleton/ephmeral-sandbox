e6fbc01e11b95a0b9d98bcb7edfe19e88d358cf5
ronen <narkisr@gmail.com>
2015-08-16 00:06:39 +0300

    adding desktop

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,3 +1,15 @@
-node 'ephmeral.local' {
+node default {
+  include apt
 
+  if($::lsbdistcodename == 'vivid'){
+    Service {
+      provider => 'systemd'
+    }
+  }
+
+  class {'desktop::x2go':
+    server => true,
+    port   => '22',
+    lan    => '10.0.2.2'
+  }
 }
