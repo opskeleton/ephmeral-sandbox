0e2f7544562153636360e6416f567f399ea25e14
ronen <narkisr@gmail.com>
2015-11-01 00:02:05 +0200

    removing chrome

--- manifests/ephmeral.pp
+++ manifests/ephmeral.pp
@@ -7,15 +7,7 @@ node default {
       provider => 'systemd'
     }
   }
-
-  class {'desktop::x2go':
-    server => true,
-    port   => '22',
-    lan    => '10.0.2.2'
-  }
-
   class{'vpnize::torguard':}
   class{'vpnize::fortint':}
   class{'vpnize::openconnect':}
-  class{'desktop::chrome':}
 }
